e4b0a1be3394f227ce4d33040fb36f11ef965c42
This1Guy-at-This1Company <this1guy@this1companyllc.com>
2025-01-07 08:21:24 -0600

    Got initial collision working for project assignment. Need to branch off to also have one that uses a triangle of circles for collision.

--- main.py
+++ main.py
@@ -3,10 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroidField = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    
+    
+    
+
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,9 +40,26 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return 
         
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill((0,0,0))
+
+        for obj in asteroids:
+           if obj.detect_collision(player):
+               print("Game over!")
+               pygame.quit()
+               return
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
